00a9310d1d9c9a2facd4b6db77717cc27eb78c02
MoZoLbKA <makurin.nik@list.ru>
2022-10-25 13:14:08 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Graphics_Chart_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.countOfInterpolatedPoints = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.chartOfPolynom = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.countOfInterpolatedPoints)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartOfPolynom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.countOfInterpolatedPoints);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1413, 48);
+            this.panel1.TabIndex = 0;
+            // 
+            // countOfInterpolatedPoints
+            // 
+            this.countOfInterpolatedPoints.Location = new System.Drawing.Point(1117, 14);
+            this.countOfInterpolatedPoints.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.countOfInterpolatedPoints.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.countOfInterpolatedPoints.Name = "countOfInterpolatedPoints";
+            this.countOfInterpolatedPoints.Size = new System.Drawing.Size(120, 22);
+            this.countOfInterpolatedPoints.TabIndex = 7;
+            this.countOfInterpolatedPoints.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.countOfInterpolatedPoints.ValueChanged += new System.EventHandler(this.countOfInterpolatedPoints_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(905, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Кол-во узлов интерполяции";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(504, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "кол-во делений(кол-во узлов + 2)";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(746, 16);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(106, 22);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button4.Location = new System.Drawing.Point(297, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 48);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "abs(LnX - fx)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.DrawError);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button3.Location = new System.Drawing.Point(193, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "max(err) : n";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.DrawDependeceCountOfInerpolationPointsAndError);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button2.Location = new System.Drawing.Point(98, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Ln_X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DrawLNX);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fx";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.DrawFX);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.chartOfPolynom);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 48);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1413, 402);
+            this.panel2.TabIndex = 1;
+            // 
+            // chartOfPolynom
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartOfPolynom.ChartAreas.Add(chartArea2);
+            this.chartOfPolynom.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.chartOfPolynom.Legends.Add(legend2);
+            this.chartOfPolynom.Location = new System.Drawing.Point(0, 0);
+            this.chartOfPolynom.Name = "chartOfPolynom";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chartOfPolynom.Series.Add(series2);
+            this.chartOfPolynom.Size = new System.Drawing.Size(1413, 402);
+            this.chartOfPolynom.TabIndex = 0;
+            this.chartOfPolynom.Text = "chart1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(1254, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1413, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.countOfInterpolatedPoints)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartOfPolynom)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartOfPolynom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown countOfInterpolatedPoints;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Graphics_Chart_
+{
+
+    public partial class Form1 : Form
+    {
+        Dictionary<double, double> x_Fx = new Dictionary<double, double>();      
+        public Form1()
+        {
+            InitializeComponent();
+            for(double i = 0; i <= 1.5; i += 0.3)
+            {
+                x_Fx.Add(i, getFx(i));
+            }
+            chartOfPolynom.ChartAreas.Add(new ChartArea("Math functions"));
+        }
+        #region "Math"
+        double getAn(double an, double qn)
+        {
+            return an * qn;
+        }
+        double getQn(double n, double x)
+        {
+            return -(Math.PI * Math.PI * Math.Pow(x, 4) * (4 * n + 1)) / (4 * (2 * n + 1) * (2 * n + 2) * (4 * n + 5));
+        }
+        
+        double getFx(double x)
+        {
+
+            double eps = 0.00001;
+            double a = x;
+            double sum = x;
+            int j = 0;
+            while (Math.Abs(a) > eps)
+            {
+                double qj = getQn(j, x);
+                sum += getAn(a, qj);
+                a = getAn(a, qj);
+                j++;
+            }
+            return sum;
+        }
+
+        List<double> GetInterpollationPoints(int n, double a, double b)
+        {
+            double h = (b - a) / n;
+            List<double> points = new List<double>();
+            for (int i = 0; i <= n; i++)
+            {
+                points.Add(a);
+                a += h;
+            }
+            return points;
+            
+        }
+        double GetLnX(double x)
+        {
+            double sum = 0;
+            List<double> points = null;
+            if (checkBox1.Checked)
+            {
+                points = GetChebyshevInterpolatedPoints(x_Fx.Count);
+            }
+            else
+            {
+                 points = GetInterpollationPoints(x_Fx.Count, 0, 1.5);
+            }
+
+            for (int i = 0; i < x_Fx.Count; i++)
+            {
+                double prod = 1;
+                for (int j = 0; j < x_Fx.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        prod *= (x - points[j]) / (points[i] - points[j]);
+                    }
+                }
+                sum += prod * getFx(points[i]);
+            }
+            return sum;
+        }
+        double GetLnX(double x,int n)
+        {
+            double sum = 0;
+            List<double> points = GetInterpollationPoints(n,0,1.5);
+            for (int i = 0; i <= n; i++)
+            {
+                double prod = 1;
+                for (int j = 0; j <= n; j++)
+                {
+                    if (i != j)
+                    {
+                        prod *= (x - points[j]) / (points[i] - points[j]);
+                    }
+                }
+                sum += prod * getFx(points[i]);
+            }
+            return sum;
+        }
+        #endregion
+        private void DrawFX(object sender, EventArgs e)
+        {
+            int n = x_Fx.Count;
+            chartOfPolynom.Series.Clear();
+            double h = (double)1.5 / n;
+            double x = 0;
+            Series mySeriesOfPoint = new Series("Функция");
+            mySeriesOfPoint.ChartType = SeriesChartType.Line;
+            mySeriesOfPoint.ChartArea = "Math functions";
+            for (int  i = 0; i <= n; i++)
+            {
+                double f = Math.Abs(getFx(x));
+                mySeriesOfPoint.Points.AddXY(x, f);
+                x += h;
+
+            }
+            chartOfPolynom.Series.Add(mySeriesOfPoint);
+        }
+
+        private void DrawLNX(object sender, EventArgs e)
+        {
+            chartOfPolynom.Series.Clear();
+            int n = (int)numericUpDown1.Value;
+            int m = (int)countOfInterpolatedPoints.Value;
+            double h = (double)1.5 / n;
+            Series mySeriesOfPoint = new Series("Полином лагранжа");
+            mySeriesOfPoint.ChartType = SeriesChartType.Line;
+            mySeriesOfPoint.ChartArea = "Math functions";
+            double x= 0;
+            for (int i = 0; i <= n; i++)
+            {
+                double f = Math.Abs(GetLnX(x,x_Fx.Count));
+                mySeriesOfPoint.Points.AddXY(x, f);
+                x += h;
+            }
+            chartOfPolynom.Series.Add(mySeriesOfPoint);
+        }
+        double GetMaxErr(int n)
+        {
+            double h = (double)1.5 / n;
+            double maxErr = 0;
+            double x = 0;
+            for (int i= 0; i< n; i++)
+            {
+                maxErr = Math.Max(maxErr, Math.Abs(GetLnX(x) - getFx(x)));
+                x += h;
+            }
+            return maxErr;
+        }
+        private void DrawDependeceCountOfInerpolationPointsAndError(object sender, EventArgs e)
+        {
+            chartOfPolynom.Series.Clear();
+            Series mySeriesOfPoint = new Series("err : n");
+            mySeriesOfPoint.ChartType = SeriesChartType.Line;
+            mySeriesOfPoint.ChartArea = "Math functions";
+
+            for (int i = 2; i < 44; i++)
+            {
+                InterpolatedPointsChanger(i);
+                 if(i%5 !=0)
+                     mySeriesOfPoint.Points.AddXY(i, GetMaxErr(i));
+                
+            }
+            chartOfPolynom.Series.Add(mySeriesOfPoint);
+        }
+
+        private void DrawError(object sender, EventArgs e)
+        {
+            chartOfPolynom.Series.Clear();
+            int n = (int)numericUpDown1.Value;
+            
+            double x = 0;
+            double h = (double)1.5 / n;
+            Series mySeriesOfPoint = new Series("Ошибка");
+            mySeriesOfPoint.ChartType = SeriesChartType.Line;
+            mySeriesOfPoint.ChartArea = "Math functions";
+            for (int i = 0; i <= n; i++)
+            {
+                double f = Math.Abs(GetLnX(x,x_Fx.Count) - getFx(x));
+                mySeriesOfPoint.LegendText = f.ToString();
+                mySeriesOfPoint.Points.AddXY(x, f);
+                x += h;
+            }            
+            chartOfPolynom.Series.Add(mySeriesOfPoint);
+            
+        }
+
+        private void InterpolatedPointsChanger(int n)
+        {
+            double h = 1.5/n;
+            x_Fx.Clear();
+            for (double i = 0; i <= 1.5; i += h)
+            {
+                x_Fx.Add(i, getFx(i));
+            }
+        }
+
+        private void countOfInterpolatedPoints_ValueChanged(object sender, EventArgs e)
+        {
+            int n = (int)countOfInterpolatedPoints.Value ;
+            double h = 1.5 / n;
+            x_Fx.Clear();
+            double x = 0;
+            if (checkBox1.Checked)
+            {
+                SetChebyshevPoints(n);
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    x_Fx.Add(x, getFx(x));
+                    x += h;
+                }
+            }
+        }
+        List<double> GetChebyshevInterpolatedPoints(int n)
+        {
+            double a = 0;
+            double b = 1.5;
+            List<double> points = new List<double>();
+            for (int i = 0; i <= n; i++)
+            {
+                points.Add((b + a) / 2 + ((b - a) / 2) * Math.Cos((2 * i + 1) * Math.PI / (2 * n + 2)));               
+            }
+            points.Reverse();
+            return points;
+        }
+        void SetChebyshevPoints(int n)
+        {
+            
+            double a = 0;
+            double b = 1.5;
+            double x;
+            for (int i = 0; i <= n; i++)
+            {
+                x = (b + a) / 2 + ((b - a) / 2) * Math.Cos((2 * i + 1)*Math.PI / (2 * n + 2));
+                x_Fx.Add(x, getFx(x));
+            }
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            x_Fx.Clear();
+            SetChebyshevPoints((int)countOfInterpolatedPoints.Value);
+        }
+    }
+}
